73f7545a1721c0d3635011c74ab0d1c00a2c3865
theyashh <itsroysons@gmail.com>
2025-11-05 23:42:00 +0530

    changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    base: '/yash-portfolio/',
+  plugins: [react()],
+  base: "/yash-portfolio/",
 });
