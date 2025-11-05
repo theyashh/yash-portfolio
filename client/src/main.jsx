c67bbb13a9668156636bcecbdbc7d2fc055cab78
theyashh <itsroysons@gmail.com>
2025-11-05 18:57:44 +0530

    deploy changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,6 +9,7 @@ import "./styles/globals.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router basename="/yash-portfolio">
+      {/* <Router> */}
       <App />
     </Router>
   </React.StrictMode>
